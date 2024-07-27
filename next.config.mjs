983351516c36e3748c3d8f7fae75ea7630d095c4
shrilathap <shrilatha.poojary.56@gmail.com>
2024-07-27 18:36:34 +0530

    Initial Commit

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ["en", "fr", "de"],
+    defaultLocale: "en",
+  },
+};
+
+export default nextConfig;
